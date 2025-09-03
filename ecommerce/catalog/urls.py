57bc867a0b57703122523e24a82b6bc452ad8cfb
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-03 12:24:30 +0330

    main_menu added , product_detail added

--- ecommerce/catalog/urls.py
+++ ecommerce/catalog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.product_list, name='main_menu') ,
-    path('detail/<int:pk>' , views.product_detail , name='product_detail')
+    path('detail/<int:pk>/' , views.product_detail , name='product_detail'),
 ]
