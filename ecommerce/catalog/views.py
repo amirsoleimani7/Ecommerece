62c47a24dddc7c849c21e986c2d76202d0cff541
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-03 11:09:41 +0330

    adding detail page

--- ecommerce/catalog/views.py
+++ ecommerce/catalog/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products' : products
+    }
+    return render(request , 'catalog/main_menu.html' , context)
 
+def product_detail(request , pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        "product" : product
+    }
+    return render(request,'catalog/detail.html',context)
