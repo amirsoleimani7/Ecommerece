65f29205cc796345542ccd993bf5fef8dbcabd3a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-09-04 11:23:57 +0330

    django admin added

--- ecommerce/hotel/admin.py
+++ ecommerce/hotel/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import GFG
 
-# Register your models here.
+
+class GFGAdmin(admin.ModelAdmin):
+    list_display = ['hotel_name' , 'hotel_price' , 'hotel_description'] # these are the fiedls that will the showen in the django.admin form
+
+admin.site.register(GFG, GFGAdmin)
+
+
+
+
--- ecommerce/hotel/views.py
+++ ecommerce/hotel/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def get_hotel(request):
-    
+
 
 
 
